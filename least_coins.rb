e9905914688641e6859de5f57cd813328e66cb77
kaitlinhbui <kaitlinhb01@gmail.com>
2018-07-26 16:49:35 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,24 +1,24 @@
-#write out your code here
+# #write out your code here
 
-cents = 30
+# cents = 30
 
-def least_coins(cents)
-  coins = {
-    :quarters => 25,
-    :dimes => 10,
-    :nickels => 5,
-    :pennies => 1
-  }
+# def least_coins(cents)
+#   coins = {
+#     :quarters => 25,
+#     :dimes => 10,
+#     :nickels => 5,
+#     :pennies => 1
+#   }
   
-  least_coins = {}
+#   least_coins = {}
   
-  total_cents = 30
+#   total_cents = 30
   
-  coins.each do |coin_type, coin_worth|
-    if #{cents} % #{coin_worth} >= 0
-      puts "quarters => 1"
-      total_cents = total_cents - #{cents} - #{coin_worth}
-  end
+#   coins.each do |coin_type, coin_worth|
+#     if #{cents} % #{coin_worth} >= 0
+#       puts "quarters => 1"
+#       total_cents = total_cents - #{cents} - #{coin_worth}
+#   end
       
 
-end
+# end
